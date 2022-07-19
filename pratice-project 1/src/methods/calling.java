2dcfa8d3e1cc44b7821188c8f664c70c9cdc66b5
saranya <vtu11119@veltech.edu.in>
2022-07-19 22:27:08 +0530

    methods

---
+++ pratice-project 1/src/methods/calling.java
@@ -0,0 +1,10 @@
+package methods;
+
+public class calling {
+	public static void main(String[] args) {
+		int a;
+		a= Math.addExact(12, 9);
+		System.out.println(a);
+	}
+
+}
---
+++ pratice-project 1/src/methods/methods.java
@@ -0,0 +1,10 @@
+package methods;
+
+public class methods {
+	public static void main(String[] agrs) {
+		multiply(9,12);
+	}
+	public static void multiply(int a , int b) {
+		System.out.println(a*b);
+	}
+}

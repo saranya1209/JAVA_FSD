988e8580664d50a44639c18228d141c746ac2ad8
saranya <vtu11119@veltech.edu.in>
2022-07-29 11:14:06 +0530

    assignment

---
+++ pratice-project 1/src/linearsearch.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class linearsearch {
+	public static void main(String[] args) {
+		int[] arr = {10,20,30,40,50};
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the element to be seached");
+		int searchValue=sc.nextInt();
+		int result=(int)linearing(arr,searchValue);
+		if(result==-1){
+			System.out.println("Element not in the array");;
+		}else {
+			System.out.println("Element found at "+result+" and the search key is"+arr[result]);
+		}
+	}
+	public static int linearing(int arr[], int X) {
+		int arrlength = arr.length;
+		for(int i=0;i<arrlength -1;i++) {
+			if(arr[i]==X) {
+				return i;
+			}
+		}
+		return-1;
+	
+
+}
+}
